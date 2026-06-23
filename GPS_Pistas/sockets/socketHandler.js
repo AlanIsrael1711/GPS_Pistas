405cea6d7f7f9b5e25db6bba3abff4bcaa8cd456
AlanIsrael1711 <136403629+AlanIsrael1711@users.noreply.github.com>
2026-06-23 16:53:45 -0600

    Add files via upload

--- GPS_Pistas/sockets/socketHandler.js
+++ GPS_Pistas/sockets/socketHandler.js
@@ -1,15 +1,12 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        // Almacenar o registrar conexiones si es necesario
-        
         socket.on('actualizar-ubicacion', (coords) => {
-            // Solo devuelve la ubicación al mismo usuario que la envió.
-            // Ningún otro cliente recibe la posición ajena.
+            // Solo devuelve la ubicación al mismo cliente
             socket.emit('dibujar-ubicacion', { id: socket.id, ...coords });
         });
 
         socket.on('disconnect', () => {
-            io.emit('usuario-desconectado', socket.id);
+            // Sin retransmisión a terceros
         });
     });
 };
